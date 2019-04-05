d077ffb22004185def1e5b12405c84d34bc61fe4
Emika Ijuin <emikaijuin@gmail.com>
2019-04-05 17:41:42 +0800

    include users and maps resource in api v1

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
+      resources :users
+      resources :maps
       post 'user_token' => 'user_token#create'
     end
   end
