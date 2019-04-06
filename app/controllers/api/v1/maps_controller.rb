4ea1ec3914c0423aeba1cd96f80beb9f7243bbf1
Emika Ijuin <emikaijuin@gmail.com>
2019-04-06 22:32:17 +0800

    fix formatting

--- app/controllers/api/v1/maps_controller.rb
+++ app/controllers/api/v1/maps_controller.rb
@@ -2,6 +2,6 @@ class Api::V1::MapsController < Api::V1::ApplicationController
   before_action :authenticate_user
 
   def index
-    render :json => { hello: "world" }
+    render json: { hello: 'world' }
   end
 end
