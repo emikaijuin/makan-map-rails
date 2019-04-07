f329406444637e3d3d42397081703cdde76f8b55
Emika Ijuin <emikaijuin@gmail.com>
2019-04-07 17:12:59 +0800

    permit params from react form

--- app/controllers/api/v1/user_token_controller.rb
+++ app/controllers/api/v1/user_token_controller.rb
@@ -2,6 +2,11 @@ module Api
   module V1
     class UserTokenController < Knock::AuthTokenController
       skip_before_action :verify_authenticity_token, raise: false
+
+      def user_params
+        # camel case from react state
+        params.require(:user).permit(:firstName, :lastName, :email, :password, :passwordConfirmation)
+      end
     end
   end
 end
