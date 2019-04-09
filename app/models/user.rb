d5bb48f68ee2bd8944ca277e9d7e2344e8cf2efa
Emika Ijuin <emikaijuin@gmail.com>
2019-04-09 11:39:37 +0800

    add basic validations for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,14 @@
 class User < ApplicationRecord
   has_secure_password
+
+  validates :email,
+    presence: true,
+    format: {
+      with: URI::MailTo::EMAIL_REGEXP,
+      message: "Please provide a valid email address."
+    }
+  validates :password, presence: true, length: {minimum: 6, message: "Password is too short (min 6 characters)."}
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+
 end
