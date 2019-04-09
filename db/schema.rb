5f79abf26417b4caf685022f4d0cddbec49b3f86
Emika Ijuin <emikaijuin@gmail.com>
2019-04-09 19:17:30 +0800

    regen schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_05_073848) do
+ActiveRecord::Schema.define(version: 2019_04_09_111307) do
+
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "listings", force: :cascade do |t|
+    t.float "latitude"
+    t.float "longitude"
+    t.text "address"
+    t.text "secondary_address"
+    t.text "zip_code"
+    t.text "city"
+    t.text "state"
+    t.text "country"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "first_name"
@@ -22,4 +36,5 @@ ActiveRecord::Schema.define(version: 2019_04_05_073848) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
 end
