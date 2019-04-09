feed63c60a41bc4f13d6a12a5ed42713b87b9197
Emika Ijuin <emikaijuin@gmail.com>
2019-04-09 11:39:27 +0800

    set http status and send to client

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,13 +2,15 @@ class Api::V1::UsersController < Api::V1::ApplicationController
   def create
     user = User.new(user_params)
 
-    message = if user.save
-                "Saved user."
-              else
-                user.errors
-              end
+    if user.save
+      message = "Saved user."
+      status = 200
+    else
+      message = user.errors
+      status = 400
+    end
 
-    render json: { "user": user, "message": message }
+    render json: { "user": user, "message": message}, status: status
   end
 
   private
