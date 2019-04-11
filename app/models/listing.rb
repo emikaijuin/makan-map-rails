793bfc31e5b20445ed0b21bef1e8f938955ac922
Emika Ijuin <emikaijuin@gmail.com>
2019-04-11 12:54:27 +0800

    WIP - Listings are given coordinates are created by user

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,4 +1,24 @@
 class Listing < ApplicationRecord
   has_many :listing_users
   has_many :users, through: :listing_users
+
+  after_create :create_coordinates
+
+  def create_coordinates
+    response = HTTParty.get(
+      'https://maps.googleapis.com/maps/api/geocode/json?' +
+      "address=" + self.geocoding_address +
+      "&key=" + Rails.application.credentials.google[:access_key]
+    )
+
+    self.lat = parsed_response.results.geometry.location.lat
+    self.long = parsed_response.results.geometry.location.long
+
+    self.save
+  end
+
+  def geocoding_address
+    # Address to send as param to Geocoding API
+    (self.address + " " + self.secondary_address + " " + self.city + " " + self.state).gsub(" ", "+")
+  end
 end
