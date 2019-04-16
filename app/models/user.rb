92fd3e1343b83719a26a9324e33b8787bc8eff67
Emika Ijuin <emikaijuin@gmail.com>
2019-04-16 17:33:40 +0800

    validate presence of username

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,7 @@ class User < ApplicationRecord
   validates :password, presence: true, length: { minimum: 6, message: "Password is too short (min 6 characters)." }
   validates :first_name, presence: true
   validates :last_name, presence: true
+  validates :username, presence: true
 
   has_many :listing_users
   has_many :listings, through: :listing_users
