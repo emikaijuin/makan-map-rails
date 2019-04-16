72898069ecdd9fe875c941595ab088b59d52559b
Emika Ijuin <emikaijuin@gmail.com>
2019-04-16 17:33:31 +0800

    Reformat how full_address is compiled and wrap API call to google maps in begin/rescue block

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -2,7 +2,7 @@ class Listing < ApplicationRecord
   has_many :listing_users
   has_many :users, through: :listing_users
 
-  after_create :create_coordinates
+  before_create :create_coordinates
 
   scope :within_bounds, ->(bounds) {
                           where("latitude < ? AND latitude > ? AND longitude > ? AND longitude < ?",
@@ -13,20 +13,27 @@ class Listing < ApplicationRecord
                         }
 
   def create_coordinates
-    response = HTTParty.get(
-      'https://maps.googleapis.com/maps/api/geocode/json?' +
-      "address=" + self.geocoding_address +
-      "&key=" + Rails.application.credentials.google[:access_key]
-    )
+    begin
+      response = HTTParty.get(
+        'https://maps.googleapis.com/maps/api/geocode/json?' +
+        "address=" + self.geocoding_address +
+        "&key=" + Rails.application.credentials.google[:access_key]
+      )
 
-    self.lat = parsed_response.results.geometry.location.lat
-    self.long = parsed_response.results.geometry.location.long
-
-    self.save
+        self.lat = response.parsed_response["results"][0].dig("geometry", "location", "lat")
+        self.long = response.parsed_response["results"][0].dig("geometry","location","lng")
+    rescue
+    end
   end
 
   def geocoding_address
     # Address to send as param to Geocoding API
-    (self.address + " " + self.secondary_address + " " + self.city + " " + self.state).gsub(" ", "+")
+    full_address = ""
+    full_address += (self.address + " ") if self.address
+    full_address += (self.secondary_address + " ") if self.secondary_address
+    full_address += (self.city + " ") if self.city
+    full_address += self.state if self.state
+
+    full_address.gsub(" ", "+")
   end
 end
