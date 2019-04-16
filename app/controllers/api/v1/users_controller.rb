b2198c14d80408a4a2252bf6e48e3cf7be8aae0e
Emika Ijuin <emikaijuin@gmail.com>
2019-04-16 17:32:37 +0800

    If given a search query, search users by query. Else, retrieve all users. Return users as json

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -13,6 +13,17 @@ class Api::V1::UsersController < Api::V1::ApplicationController
     render json: { "user": user, "message": message }, status: status
   end
 
+  def index
+    sql_query = "%#{params[:query]}%"
+    @users =  if params[:query]
+                User.where("first_name ILIKE ? OR last_name ILIKE ? OR username ILIKE ?", sql_query, sql_query, sql_query)
+              else
+                User.all
+              end
+
+    render json: @users, status: status
+  end
+
   private
 
   def user_params
